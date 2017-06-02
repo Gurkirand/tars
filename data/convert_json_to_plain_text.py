5c5f9c3fdc40f949c564c5b4117ffb270233dfd9
Gurkiran Dhaliwal <gurkirand@gmail.com>
2017-06-02 14:51:36 -0700

    Fixed convert json to text.

--- data/convert_json_to_plain_text.py
+++ data/convert_json_to_plain_text.py
@@ -1,6 +1,15 @@
-with open("reddit_jokes.json") as f:
-	i = 0
-	for line in f:
-		if i > 50: break
-		print line
-		i += 1
+import json
+
+with open("reddit_jokes.json", "r") as f:
+    jokes = json.load(f)
+
+plaintext = []
+counter = 0
+for joke in jokes:
+    text = joke["title"].replace("\n", " ") + " " + joke["body"].replace("\n", " ") + "\n"
+    plaintext.append(text)
+    
+with open("reddit_jokes.txt", "w") as f:
+    for line in plaintext:
+        f.write(line.encode('utf8'))
+        
