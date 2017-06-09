1922111c2ffa962c743f64ed443b2805f7ba773f
Ankur Kumar Banerjee <ankurbanerjee@gmail.com>
2017-06-09 13:48:36 -0700

    Some fucking changes

--- keras_LSTM.py
+++ keras_LSTM.py
@@ -20,8 +20,8 @@ import numpy as np
 import random
 import sys
 
-path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
-text = open(path).read().lower()
+# path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
+text = open('data/reddit_jokes.txt').read().lower()
 print('corpus length:', len(text))
 
 chars = sorted(list(set(text)))
@@ -51,7 +51,7 @@ for i, sentence in enumerate(sentences):
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(LSTM(512, input_shape=(maxlen, len(chars))), regu)
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
@@ -73,9 +73,7 @@ for iteration in range(1, 60):
     print()
     print('-' * 50)
     print('Iteration', iteration)
-    model.fit(X, y,
-              batch_size=128,
-              epochs=1)
+    model.fit(X, y, batch_size=32, epochs=1)
 
     start_index = random.randint(0, len(text) - maxlen - 1)
 
